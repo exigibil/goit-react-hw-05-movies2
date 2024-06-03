0fcd35f8143601e5db0b7e6264967e7cd19bc372
exigibil <adrian.topa04@gmail.com>
2024-06-03 21:14:26 +0300

    update css mobile view

--- src/components/ContentSite/Navbar/Navbar.jsx
+++ src/components/ContentSite/Navbar/Navbar.jsx
@@ -5,7 +5,8 @@ import { SiThemoviedatabase } from "react-icons/si";
 function Navbar() {
   return (
     <div className={styles.navContainer}>
-      <SiThemoviedatabase className={styles.navlogo} />
+      <div><Link to="/"><SiThemoviedatabase className={styles.navlogo} /></Link></div>
+
       <div className={styles.navlinkContainer}>
         <div className={styles.navlink}><Link to="/">Home</Link></div>
         <div className={styles.navlink}><Link to="/movies">Movies</Link></div>
--- src/components/ContentSite/SearchBar/SearchBar.jsx
+++ src/components/ContentSite/SearchBar/SearchBar.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import { SlMagnifier } from "react-icons/sl";
-import styles from "./SearchBar.module.css";
-import PropTypes from "prop-types";
+import React, { useState } from 'react';
+import { SlMagnifier } from 'react-icons/sl';
+import styles from './SearchBar.module.css';
+import PropTypes from 'prop-types';
 
 function SearchBar({ onSearch }) {
-  const [searchItem, setSearchItem] = useState("");
+  const [searchItem, setSearchItem] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     onSearch(searchItem);
   };
@@ -16,17 +16,22 @@ function SearchBar({ onSearch }) {
       <div className={styles.appName}>AEM Search Movies</div>
       <div className={styles.searchBarContainer}>
         <form className={styles.searchBarForm} onSubmit={handleSubmit}>
-          <button type="submit" className={styles.button}>
-            <SlMagnifier className={styles.searchIcon} />
-          </button>
-          <input
-            type="text"
-            className={styles.searchBarInput}
-            placeholder="Search for movies..."
-            value={searchItem}
-            onChange={(e) => setSearchItem(e.target.value)}
-            aria-label="Search movies"
-          />
+          <div>
+            {' '}
+            <button type="submit" className={styles.button}>
+              <SlMagnifier className={styles.searchIcon} />
+            </button>
+          </div>
+          <div>
+            <input
+              type="text"
+              className={styles.searchBarInput}
+              placeholder="Search for movies..."
+              value={searchItem}
+              onChange={e => setSearchItem(e.target.value)}
+              aria-label="Search movies"
+            />
+          </div>
         </form>
       </div>
     </div>
